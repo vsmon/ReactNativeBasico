84b156d75801d59b2deed920c8773a731be708d4
Rodrigo F Martins <rodrigo.fonsecamartins@gmail.com>
2018-12-26 17:52:18 -0200

    alteracoes state

--- PrimeiroProjeto/App.js
+++ PrimeiroProjeto/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+class Campos extends Component{
+  constructor(props){
+    super(props);
+    this.state = {rua:''};
+    
+    let {logradouro} = consultaCep('18022252'); 
+   
+    let texto = 'lasdflas';
+    ()=>{this.setState({rua:'texto'})};
+    
+  }
+  componentDidMount(){
+    ()=>{this.setState({rua:'asldfjklasj'})};
+  }
+  render(){
+    return(      
+      <View style={styles.container}>
+        <TextInput keyboardType='numeric' placeholder='Digite um Cep' />
+        <Text>CEP:</Text>
+        <Text>Rua:{this.state.rua}</Text>
+        <Text>UF:</Text>
+        <Text>Cidade:</Text>
+        <Button title="Consulta Cep" onPress={()=>this.setState({})}/>
+        
+      </View>
+    );
+  }
+    
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Campos/>
     );
   }
 }
@@ -19,3 +47,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+async function consultaCep(pCep){
+  /* viacep.com.br/ws/01001000/json/  */
+  const url = `https://viacep.com.br/ws/${pCep}/json/`;
+  const resposta = await fetch(url);
+  const json = await resposta.json();
+  console.log(json);
+  return json;
+}
